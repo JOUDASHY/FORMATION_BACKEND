fd1a2579c1ff9a0b37c7cb17a77c7a747545af32
JOUDASHY <alitsiryeddynilsen@gmail.com>
2025-01-30 18:10:02 +0300

    pwd no confirmation on register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -52,7 +52,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed', // Ajout de confirmation du mot de passe
+            'password' => 'required|string|min:6', // Ajout de confirmation du mot de passe
             'sex' => 'required|string',
         ]);
     
